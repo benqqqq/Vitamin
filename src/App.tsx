cf4b45faf7481f528cba216d8ab4a596b604fec2
mitchwu <benq7879@gmail.com>
2023-06-20 11:59:30 +0800

    build: add deployment github workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
 import { Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getConfig } from './config'
 
 export default function App(): ReactElement {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={getConfig().basename}>
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
 					<Route path='/' element={undefined} />
